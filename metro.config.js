0d8e02885b6299ab31c3f0f5e589bc54d8730267
unknown <mudasirkha450@gmail.com>
2025-12-24 09:45:03 +0500

    Remove wrong directory

--- metro.config.js
+++
@@ -1,7 +0,0 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require('nativewind/metro');
-// import "./app/globals.css"
-// import "./app/globals.csss"
-const config = getDefaultConfig(__dirname)
- 
-module.exports = withNativeWind(config, { input: "./app/globals.css" })
